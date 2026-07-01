bf10e0d01ba6d841608d9015f8fb8763b1818fa0
min-00702 <25035792@myrp.edu.sg>
2026-07-01 19:36:34 +0800

    added a home page for when a new user visits the website

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -7,6 +7,6 @@ from flask import Blueprint, render_template
 dashboard_bp = Blueprint("dashboard", __name__, template_folder="templates")
 
 
-@dashboard_bp.route("/")
+@dashboard_bp.route("/dashboard")
 def home():
     return render_template("dashboard/home.html")

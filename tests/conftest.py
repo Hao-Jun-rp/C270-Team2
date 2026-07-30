f90623033110c9d202a8e317b20cd0be6fbb7160
iwannacrashoutrn69 <matthewng3508@gmail.com>
2026-07-30 21:50:40 +0800

    Add pytest tests for reviews feature

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import os
+import pytest
+
+os.environ["DATABASE_URL"] = "sqlite:///:memory:"
+
+from app import create_app
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+
+    app.config.update({
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+    })
+
+    yield app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
